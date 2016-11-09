d046a82cb3aa0caada58f516194a5a98943c4120
Yueying Chen <yueych@microsoft.com>
2016-11-09 15:46:06 +0800

    refactor UI

--- GeneratorUI.py
+++ GeneratorUI.py
@@ -4,24 +4,37 @@
 import Generator
 import Constants
 import Tkinter
+import subprocess
+import os
+
 
 def getEntry():
     return entry.get()
+
 
 def generateAll():
     generator = Generator.Generator(Constants.TypeFeatureAll, getEntry())
     generator.execute()
 
+
 def generateBuilder():
     generator = Generator.Generator(Constants.TypeBuilder, getEntry())
     generator.execute()
 
+
+def openExplore():
+    subprocess.Popen(r'explorer /select,"./"')
+
+
 root = Tkinter.Tk()
+message = Tkinter.StringVar()
+hintMes = Tkinter.Label(root, textvariable=message).pack()
+message.set("Enter Name: ")
 
 entry = Tkinter.Entry(root)
-buttonAll = Tkinter.Button(root, text="All", command= generateAll)
-buttonBuilder = Tkinter.Button(root, text="Builder", command= generateBuilder)
 entry.pack()
-buttonAll.pack()
+buttonAll = Tkinter.Button(root, text="All", command=generateAll).pack()
+buttonBuilder = Tkinter.Button(root, text="Builder", command=generateBuilder).pack()
+buttonOpenExplore = Tkinter.Button(root, text="Open Folder", command=openExplore).pack()
 
 root.mainloop()
